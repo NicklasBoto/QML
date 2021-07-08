7ff1d862792a25c3113368fb058e0cf6812a957d
Nicklas Botö <nicklas.boto@gmail.com>
2021-07-08 20:58:58 +0200

    Add error messages

--- src/Type/Error.hs
+++ src/Type/Error.hs
@@ -8,10 +8,25 @@ data TypeError
     | InnerProductNotDefined Expr Expr
     | VariableNotInScope String
     | UnexpectedError
-    deriving Show
 
 instance Semigroup TypeError where
     e <> _ = e
 
 instance Monoid TypeError where
     mempty = UnexpectedError
+
+instance Show TypeError where
+    show (TypeMismatch expected actual) =
+        "Couldn't match expected type '" ++ show expected ++ "' with actual type '" ++ show actual
+
+    show (NotOrthogonal t f) =
+        "Quantum conditional branches must be orthogonal, ⟨" ++ show t ++ "|" ++ show f ++ "⟩ ≠ 0"
+
+    show (InnerProductNotDefined u v) =
+        "Inner product is not defined for given expressions, ⟨" ++ show u ++ "|" ++ show v ++ "⟩ : ?"
+
+    show (VariableNotInScope v) =
+        "Variable '" ++ v ++ "' not in scope"
+
+    show UnexpectedError =
+        "Something unexpected happened!"
